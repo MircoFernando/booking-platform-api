d9368f4a1ebcc3fac3fd46806f5a71db7e54f376
MircoFernando <mircofdo@gmail.com>
2026-07-10 11:55:56 +0530

    feat: health check endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealth() {
+    return {
+      status: 'ok',
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString(),
+      message: 'EN2H Booking Platform API is running smoothly.'
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Apply the global prefix
+  app.setGlobalPrefix('api/v1');
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}/api/v1`);
 }
 bootstrap();
