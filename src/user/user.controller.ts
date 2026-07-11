5e9ef3b73beccceca343e279f4ced71a30f0db1a
MircoFernando <mircofdo@gmail.com>
2026-07-11 13:17:00 +0530

    fix: query parameter in user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Param, Query, ParseUUIDPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
-  @Get(':id')
-  async getUser(@Param('id', ParseUUIDPipe) id: string) {
+  @Get('find')
+  async getUser(@Query('id', ParseUUIDPipe) id: string) {
     return this.userService.findOne(id);
   }
 
